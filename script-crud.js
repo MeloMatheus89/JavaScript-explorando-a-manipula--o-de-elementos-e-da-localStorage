67c36ae9cf1d97e6756feb0e551b3dd2cb5c6b58
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-05-16 10:35:46 -0300

    criado o markdown do caderno

--- script-crud.js
+++ script-crud.js
@@ -1,7 +1,22 @@
 // Encontrando o botão
 
-const btnAdicionarTarefa = docmeunt.querySelector(".app__button--add-task");
-
+const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
+const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textArea = document.querySelector(".app__form-textarea");
+// Para ficar mais fácil, vamos usar listaTarefas para não confundir com tarefas da part do textArea.
+const listaTarefas = [];
 btnAdicionarTarefa.addEventListener("click", () => {
   // Adicionar os elementos da tarefa
+  formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const tarefa = {
+    descricao: textArea.value,
+  };
+  listaTarefas.push(tarefa);
+  // Usando o localStorage
+  //localStorage.setItem("tarefas", listaTarefas); <--> Neste momento o objeto estaria do tipo: [{descricao: 'texto digitado'}].toString() e isso retorna um '[object Object]'. Que claramente foge do que queremos. Visto que o local Storage só armazena strings.
+  localStorage.setItem("tarefas", JSON.stringify(listaTarefas));
 });
