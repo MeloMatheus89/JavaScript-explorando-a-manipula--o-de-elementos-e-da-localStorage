788eec262ded6cc7a313083a80df4f9b1e9864ee
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-05-20 15:00:44 -0300

    adicionando as classes seguindo a referencia "elemento li"

--- script-crud.js
+++ script-crud.js
@@ -4,33 +4,36 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textArea = document.querySelector(".app__form-textarea");
 // Para ficar mais fácil, vamos usar listaTarefas para não confundir com tarefas da part do textArea.
-const listaTarefas = [];
+const listaTarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+const ulTarefas = document.querySelector(".app__section-task-list");
 
 function criarElementoTarefa(tarefa) {
   //devolver um html que represente essa tarefa;
   const li = document.createElement("li");
-  li.document.classList.add("app__section-task-list-item");
+  li.classList.add("app__section-task-list-item");
 
   const svg = document.createElement("svg");
   svg.innerHTML = `
-    <svg>
-      <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-        <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-      </svg>
-    </svg>
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+      <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+    </svg>    
   `;
   const paragrafo = document.createElement("p");
   paragrafo.textContent = tarefa.descricao;
+  paragrafo.classList.add("app__section-task-list-item-description");
 
   const botao = document.createElement("button");
   const imagemDoBotao = document.createElement("img");
   imagemDoBotao.setAttribute("src", "/imagens/edit.png");
+  botao.classList.add("app_button-edit");
 
   botao.append(imagemDoBotao);
   li.append(svg);
   li.append(paragrafo);
   li.append(botao);
+
+  return li;
 }
 btnAdicionarTarefa.addEventListener("click", () => {
   // Adicionar os elementos da tarefa
@@ -46,6 +49,11 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
   // Usando o localStorage
   //localStorage.setItem("tarefas", listaTarefas); <--> Neste momento o objeto estaria do tipo: [{descricao: 'texto digitado'}].toString() e isso retorna um '[object Object]'. Que claramente foge do que queremos. Visto que o local Storage só armazena strings.
   localStorage.setItem("tarefas", JSON.stringify(listaTarefas));
+});
+
+listaTarefas.forEach((tarefa) => {
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
 });
 
 // Manter no final do arquivo o texto abaixo
